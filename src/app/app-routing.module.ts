1b3d439c324ec4028ff42993b535ef779ff21786
Altamash Saqib <asaqib@altimetrik.com>
2020-03-30 13:52:54 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddbookComponent } from './addbook/addbook.component';
+import { ShowbookComponent } from './showbook/showbook.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'addbook', component: AddbookComponent},
+  {path: 'showbook',component: ShowbookComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [AddbookComponent,ShowbookComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+/*import { UserComponent } from './user.component';*/
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { ShowbookComponent } from './showbook/showbook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    /*UserComponent,*/
+    NavbarComponent,
+    AddbookComponent,
+    ShowbookComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
